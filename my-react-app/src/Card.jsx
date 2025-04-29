c84d70d2fd105236dd91a3f86f7a75651578c216
Vasisthayadav2123 <vasisthayadav58@gmail.com>
2025-04-29 16:59:34 +0530

    making card component

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function card() {
+  return (
+    <div className="card">
+      <img src="https://placehold.jp/150x150.png" alt="" className="cardimage" />
+      <h2>Card Title</h2>
+      <p>This is a description of the card. It contains some information about the content.</p>
+    </div>
+  );
+    
+}
+export default card
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+      <h1>My React App</h1>
+      <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <hr></hr>
+        </ul>
+      </nav>
+      </div>
+    </header>
+  );
+}
+export default Header
